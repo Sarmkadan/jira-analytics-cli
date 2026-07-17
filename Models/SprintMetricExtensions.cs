168d54d80bcea3c6bce22b341a0c3bf81ecf370e
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 16:48:28 +0300

    refactor: polish SprintMetricExtensions

--- Models/SprintMetricExtensions.cs
+++ Models/SprintMetricExtensions.cs
@@ -7,9 +7,16 @@ public static class SprintMetricExtensions
     /// </summary>
     /// <param name="sprintMetric">The sprint metric.</param>
     /// <returns>The sprint progress as a percentage.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="sprintMetric"/> is null.</exception>
+    /// <exception cref="DivideByZeroException">Thrown when <paramref name="sprintMetric"/>.PlannedStoryPoints is zero.</exception>
     public static double GetProgressPercentage(this SprintMetric sprintMetric)
     {
         ArgumentNullException.ThrowIfNull(sprintMetric);
+
+        if (sprintMetric.PlannedStoryPoints == 0)
+        {
+            throw new DivideByZeroException("PlannedStoryPoints cannot be zero for progress calculation.");
+        }
 
         return (double)sprintMetric.CompletedStoryPoints / sprintMetric.PlannedStoryPoints * 100;
     }
@@ -19,6 +26,7 @@ public static class SprintMetricExtensions
     /// </summary>
     /// <param name="sprintMetric">The sprint metric.</param>
     /// <returns>True if the sprint is complete; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="sprintMetric"/> is null.</exception>
     public static bool IsSprintComplete(this SprintMetric sprintMetric)
     {
         ArgumentNullException.ThrowIfNull(sprintMetric);
@@ -31,11 +39,18 @@ public static class SprintMetricExtensions
     /// </summary>
     /// <param name="sprintMetric">The sprint metric.</param>
     /// <returns>The average story points completed per day.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="sprintMetric"/> is null.</exception>
+    /// <exception cref="DivideByZeroException">Thrown when the sprint duration is zero days.</exception>
     public static double GetAverageDailyProgress(this SprintMetric sprintMetric)
     {
         ArgumentNullException.ThrowIfNull(sprintMetric);
 
         var daysElapsed = (sprintMetric.EndDate - sprintMetric.StartDate).Days;
+        if (daysElapsed == 0)
+        {
+            throw new DivideByZeroException("Sprint duration cannot be zero days.");
+        }
+
         return (double)sprintMetric.CompletedStoryPoints / daysElapsed;
     }
 }
