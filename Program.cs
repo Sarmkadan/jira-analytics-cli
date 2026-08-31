5ebfc4ae8a1911359a67c5926c59fe9dee70c402
Vladyslav Zaiets <vladyslav.zaiets@example.com>
2026-08-31 07:33:06 +0000

    chore: remove unused ConfigureServices method from Program.cs

--- Program.cs
+++ Program.cs
@@ -38,51 +38,7 @@ class Program
         }
     }
 
-    static void ConfigureServices(IServiceCollection services)
-    {
-        // Logging configuration
-        services.AddLogging(builder =>
-        {
-            builder.AddConsole();
-            builder.SetMinimumLevel(LogLevel.Information);
-        });
-
-        // Configuration
-        services.AddSingleton<IConfigurationProvider, AppConfigurationProvider>();
-        services.AddSingleton<ICliConfig>(sp =>
-        {
-            var provider = sp.GetRequiredService<IConfigurationProvider>();
-            return provider.LoadConfiguration();
-        });
-
-        // HTTP client for Jira API
-        services.AddHttpClient("jira")
-            .ConfigureHttpClient((sp, client) =>
-            {
-                var config = sp.GetRequiredService<ICliConfig>();
-                client.BaseAddress = new Uri(config.JiraBaseUrl);
-                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {config.JiraApiToken}");
-                client.DefaultRequestHeaders.Add("Accept", "application/json");
-            });
-
-        // Data access layer
-        services.AddSingleton<IIssueRepository, IssueRepository>();
-        services.AddSingleton<ISprintRepository, SprintRepository>();
-        services.AddSingleton<IMetricsRepository, MetricsRepository>();
-
-        // Business logic layer
-        services.AddSingleton<IJiraApiService, JiraApiService>();
-        services.AddSingleton<IAnalyticsService, AnalyticsService>();
-        services.AddSingleton<IReportService, ReportService>();
-        services.AddSingleton<IExportService, ExportService>();
-    services.AddSingleton<ICsvExportService, CsvExportService>();
-        services.AddSingleton<IJqlQueryService, JqlQueryService>();
-        services.AddSingleton<IHtmlReportService, HtmlReportService>();
-    services.AddSingleton<IMarkdownReportService, MarkdownReportService>();
-        services.AddSingleton<ITeamComparisonService, TeamComparisonService>();
-        services.AddSingleton<ISnapshotStore, SnapshotStore>();
-    }
-
+    
     static RootCommand BuildRootCommand(IServiceProvider serviceProvider)
     {
         var rootCommand = new RootCommand("Jira Analytics CLI - Advanced sprint and team metrics analysis");
